8e1d5b5d11b50dc39825e6e8ea2d52f0abdfd3f6
Sergiu Pantiru <12131904+sergiupantiru@users.noreply.github.com>
2020-11-24 21:55:45 +0100

    add get command

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -21,17 +22,46 @@ type Repository struct {
 	path string
 }
 
+//NewRepository returns a new instance of chart repository
 func NewRepository(path string) *Repository {
 	return &Repository{
 		path: path,
 	}
 }
 
-func init() {
+//List returns a list of charts from repository
+func (r *Repository) List(filterString string) (ret []Chart, err error) {
+	repo, err := r.load()
+	if err != nil {
+		return nil, err
+	}
 
+	for _, c := range repo.Charts {
+		if strings.Contains(strings.ToLower(c.Name), strings.ToLower(filterString)) {
+			ret = append(ret, c)
+		}
+	}
+
+	return ret, err
 }
 
-func (r *Repository) List(filterString string) (ret []Chart, err error) {
+//Get returns a charts entity from repository, if exists
+func (r *Repository) Get(name string) (ret *Chart, err error) {
+	repo, err := r.load()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, c := range repo.Charts {
+		if strings.ToLower(c.Name) == strings.ToLower(name) {
+			return &c, nil
+		}
+	}
+
+	return nil, errors.New("no chart with name " + name + " found in repository")
+}
+
+func (r *Repository) load() (*Repo, error) {
 	jsonFile, err := os.Open(r.path)
 	if err != nil {
 		return nil, err
@@ -41,12 +71,5 @@ func (r *Repository) List(filterString string) (ret []Chart, err error) {
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 	var repo Repo
 	json.Unmarshal(byteValue, &repo)
-
-	for _, c := range repo.Charts {
-		if strings.Contains(strings.ToLower(c.Name), strings.ToLower(filterString)) {
-			ret = append(ret, c)
-		}
-	}
-
-	return ret, err
+	return &repo, nil
 }
--- pkg/repo_client.go
+++ pkg/repo_client.go
@@ -1,26 +1,31 @@
 package pkg
 
 import (
+	"fmt"
 	"io"
+	"os"
 
 	"github.com/amacami/dc-charts/internal/repository"
 	"github.com/olekukonko/tablewriter"
 	"github.com/prometheus/common/log"
 )
 
+//RepoClient
 type RepoClient struct {
 	repository *repository.Repository
 }
 
+//NewRepoClient returns new instance of the repository client
 func NewRepoClient() *RepoClient {
 	return &RepoClient{
-		repository: repository.NewRepository("repository/repo.json"),
+		repository: repository.NewRepository("testdata/repo.json"),
 	}
 }
+
+//ListRepoEntries prints a list of all charts from repository
 func (c *RepoClient) ListRepoEntries(out io.Writer, filter string) error {
 	list, err := c.repository.List(filter)
 	if err != nil {
-		log.Error(err)
 		return err
 	}
 	table := tablewriter.NewWriter(out)
@@ -31,4 +36,22 @@ func (c *RepoClient) ListRepoEntries(out io.Writer, filter string) error {
 	}
 	table.Render()
 	return err
+}
+
+//Get downloads the DC files for the provided chart name
+func (c *RepoClient) Get(out io.Writer, name string) error {
+	chart, err := c.repository.Get(name)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
+	fileName := "testdata/" + chart.Url
+	file, err := os.Stat(fileName)
+	if os.IsNotExist(err) {
+		return fmt.Errorf("cannot download chart from url %s", fileName)
+	}
+
+	fmt.Println("File to copy " + file.Name())
+	return nil
 }
